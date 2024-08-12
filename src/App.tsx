5681d879054f692a5c81bbafe0694c174a264cba
Erin Beachkofski <erin.beachkofski@abbott.com>
2024-08-12 09:10:17 -0700

    add basic plant component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import { Plant } from "./Plant";
+
 export const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <div className="w-screen h-screen bg-yellow-900	">
-          <h1 className="w-full text-3xl font-bold text-green-600 text-center">
+        <div className="w-screen h-screen bg-yellow-900">
+          <div className="w-full text-3xl font-bold text-center text-green-600">
             Welcome to erin's garden!
-          </h1>
+          </div>
+          <div className="w-full font-bold text-center text-green-600 text-md">{`cultivating silly simplicity :)`}</div>
+          <Plant />
         </div>
       </header>
     </div>
---
+++ src/Plant.tsx
@@ -0,0 +1,3 @@
+export const Plant = () => {
+  return <div className="absolute w-10 h-10 bg-green-600 top-10 left-10"></div>;
+};
