a9ee54296d5a4c582533a8c53224e6df5ac91322
Erin Beachkofski <erin.beachkofski@abbott.com>
2024-08-14 15:00:03 -0700

    fix plant size on mobile

--- src/Plant.tsx
+++ src/Plant.tsx
@@ -4,7 +4,7 @@ export const Plant = () => {
   const randomXOffset = Math.random() * 40;
   const randomYOffset = Math.random() * 40;
   console.log(`${randomXOffset}, ${randomYOffset}`);
-  const dynamicClassName = `h-10 h-10 sm:w-16 sm:h-16 relative hover:scale-110 transform transition-transform duration-300 ease-in-out`;
+  const dynamicClassName = `w-10 h-10 sm:w-16 sm:h-16 relative hover:scale-110 transform transition-transform duration-300 ease-in-out`;
 
   return (
     <div
