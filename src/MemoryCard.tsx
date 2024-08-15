3f294762cce2cfc252d1fda6834f99b44772ceb9
Erin Beachkofski <erbeach527@gmail.com>
2024-08-15 16:23:29 -0700

    cleanup code

--- src/MemoryCard.tsx
+++ src/MemoryCard.tsx
@@ -13,7 +13,6 @@ export const MemoryCard = ({ show, setShow }: MemoryCardProps) => {
     "Big baking day! OMG I made blueberry flaxseed oatmeal muffins… but I forgot the blueberries!!!! So I made a blueberry compote instead and was able to salvage the muffins (they were honestly really good - not too sweet). ";
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
-      console.log(event.target);
       if (
         cardBackgroundRef.current &&
         cardBackgroundRef.current.contains(event.target as Node) &&
@@ -41,25 +40,21 @@ export const MemoryCard = ({ show, setShow }: MemoryCardProps) => {
       <div
         ref={cardBackgroundRef}
         className="absolute top-0 left-0 flex items-center justify-center w-full h-full bg-zinc-100"
-        style={{ backgroundColor: "rgba(63, 63, 70, 0.25)" }} // Use RGBA for transparency
+        style={{ backgroundColor: "rgba(63, 63, 70, 0.25)" }}
       >
         <div
           ref={cardRef}
           className="flex flex-col h-auto px-6 py-6 shadow-lg w-72 rounded-xl bg-amber-50"
         >
-          <div className="flex flex-row justify-between">
-            <text className="text-xl text-zinc-700">
-              {new Date(Date.now()).toLocaleDateString("en-US", {
-                year: "numeric",
-                month: "long",
-                day: "numeric",
-              })}
-            </text>
+          <div className="flex flex-row justify-between text-xl text-zinc-700">
+            {new Date(Date.now()).toLocaleDateString("en-US", {
+              year: "numeric",
+              month: "long",
+              day: "numeric",
+            })}
             <img src={PlantImg} alt="plant" className="h-14" />
           </div>
-          <div className="pt-10">
-            <text className="text-md text-zinc-700">{memoryText}</text>
-          </div>
+          <div className="pt-10 text-md text-zinc-700">{memoryText}</div>
         </div>
       </div>
     </div>
